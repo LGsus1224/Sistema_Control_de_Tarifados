29bb0b9e634f38d1381e56a2cebce4efdcacd3e6
UwUrYisus <109441967+UwUrYisus@users.noreply.github.com>
2023-12-07 18:12:32 -0500

    Mi primero commit blueprint
    Nose como hacerlo :cccc

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,10 @@
-from flask import Flask, redirect, url_for
-
+from flask import Flask
+from app.routes import login_bp
 from app.libs import *
 # DB MODELS
 from app.models import *
 
+#Función para instanciar y configurar la aplicación
 
 def create_app(settings_module):
     app = Flask(__name__, instance_relative_config=True)
@@ -12,5 +13,7 @@ def create_app(settings_module):
     # Libraries initialization
     db.init_app(app)
     login_manager.init_app(app)
-
-    return app
+   
+    #Registrar Blueprints de la app
+    app.register_blueprint(login_bp)
+    return app #Retorna aplicación Flask 
